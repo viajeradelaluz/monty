2418013ec562c44758f5477f424e31bd8de5a7f1
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-11 22:48:38 -0500

    feat: set the structure of the opcode

--- monty.c
+++ monty.c
@@ -1,13 +1,14 @@
 #include "monty.h"
 
-int main(size_t argc, char *argv[])
+/**
+ *
+ *
+ */
+int main(int argc, char *argv[])
 {
-	char *file_name = NULL;
-	int i;
-	char buffer[3000];
-	char *token = NULL;
-	FILE *file_pointer = NULL;
-
+	char *monty_file = NULL, *token = NULL;
+	FILE *monty_opened = NULL;
+	char buffer[BUFSIZ];
 
 	/*Checks number of arguments*/
 	if (argc != 2)
@@ -15,27 +16,27 @@ int main(size_t argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file_name = argv[1];
-	file_pointer = fopen(file_name, "r");
-	if (!file_pointer)
+
+	/* Open and read the monty file */
+	monty_file = argv[1];
+	monty_opened = fopen(monty_file, FO_RDONLY);
+	if (!monty_opened)
 	{
-		fprintf(stderr, "Error: Can't open file %s", file_name);
+		fprintf(stderr, "Error: Can't open file %s", monty_file);
 		exit(EXIT_FAILURE);
 	}
-	while (fgets(buffer, sizeof(buffer), file_pointer)!= NULL)
+
+	/* Get and parse the lines of the monty file */
+	while (fgets(buffer, sizeof(buffer), monty_opened) != NULL)
 	{
-		token = NULL;
 		token = strtok(buffer, " \n\t");
 		while (token != NULL)
 		{
-			fprintf(stdout, "%s \n", token);
-
-			token = strtok(NULL, " ");
-		}		
+			fprintf(stdout, "%s", token);
+			token = strtok(NULL, " \n\t");
+		}
 	}
-	
-	
 
-	fclose(file_pointer);
+	fclose(monty_opened);
 	return (0);
 }
